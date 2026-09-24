67152ad7f2fb068b86c55d681f35e7af20374408
mpflo <qipeot@gmail.com>
2026-09-24 20:21:20 -0300

    adiciona textos com numeros convertidos

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let numInput = document.getElementById("num-input")
 const btnConvert = document.getElementById("btn-convert")
-
+let lengthEl = document.getElementById("length")
+let volumeEl = document.getElementById("volume")
+let massEl = document.getElementById("mass")
 
 btnConvert.addEventListener("click", function() {
     let num = Number(numInput.value)
@@ -20,6 +22,10 @@ function convertValues(value) {
     const liters = value / 0.264
     const pounds = value * 2.204
     const kg = value / 2.204
+
+    lengthEl.textContent = `${value} meters = ${feet.toFixed(3)} feet | ${value} feet = ${meters.toFixed(3)} meters`
+    volumeEl.textContent = `${value} liters = ${gallons.toFixed(3)} gallons | ${value} gallons = ${liters.toFixed(3)} liters`
+    massEl.textContent = `${value} kilos = ${pounds.toFixed(3)} pounds | ${value} pounds = ${kg.toFixed(3)} kilos`
     
 }
 
