10a374122a3b9b535d44601b6492a1202913e3e2
mpflo <qipeot@gmail.com>
2026-09-23 22:54:33 -0300

    adiciona função para conversão de numeros e dos cálculos

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
+let numInput = document.getElementById("num-input")
+const btnConvert = document.getElementById("btn-convert")
+
+
+btnConvert.addEventListener("click", function() {
+    let num = Number(numInput.value)
+    convertValues(num)
+})
+
+function convertValues(value) {
+    /*
+        1 meter = 3.281 feet
+        1 liter = 0.264 gallon
+        1 kilogram = 2.204 pound
+    */
+
+    const feet = value * 3.281
+    const meters = value / 3.281
+    const gallons = value * 0.264
+    const liters = value / 0.264
+    const pounds = value * 2.204
+    const kg = value / 2.204
+    
+}
+
+
